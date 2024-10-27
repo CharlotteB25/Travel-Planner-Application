742404740d776ef06b9309454fe6d0bc25a460fd
Charlotte Billiet <charbill@student.arteveldehs.be>
2024-10-27 20:15:11 +0100

    deploy

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,49 +5,33 @@ import mongoose from "mongoose";
 import app from "./app";
 import { Server } from "http";
 
-const port: number = parseInt(process.env.PORT ?? "3002");
+const port: number = parseInt(process.env.PORT ?? "3000");
 
-//connect to mongo
-if (process.env.MONGO_CONNECTION) {
-  mongoose
-    .connect(process.env.MONGO_CONNECTION)
-    .then(() => {
-      console.log("Connected to MongoDB");
-
-      // start server
-      const server = app.listen(port, () => {
-        console.log(`Server is running on port http://localhost:${port}`);
-      });
-
-      /*const newDocument = new TripModel({
-        title: "Trip to Paris",
-        location: "Paris",
-        startDate: new Date("2022-01-01"),
-        endDate: new Date("2022-01-10"),
-        notes: "A trip to Paris",
-        expenses: "1000",
-        activity: "Sightseeing",
-        userId: "66c9d365445b9dcad00e39fd",
-      });
-
-      newDocument
-        .save()
-        .then((doc) => {
-          console.log("Document saved:", doc);
-        })
-        .catch((err) => {
-          console.error("Error saving document:", err);
-        });
-        */
-
-      process.on("SIGINT", () => stopServer(server));
-      process.on("SIGTERM", () => stopServer(server));
-    })
-    .catch((error) => console.error(error));
-} else {
-  throw new Error("No MongoDB connection string");
+if (!process.env.MONGO_CONNECTION) {
+  console.error("MongoDB connection string is missing!");
+  process.exit(1);
 }
-// stop server
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_CONNECTION)
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    // Start server
+    const server = app.listen(port, () => {
+      console.log(`Server is running on port http://localhost:${port}`);
+    });
+
+    process.on("SIGINT", () => stopServer(server));
+    process.on("SIGTERM", () => stopServer(server));
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  });
+
+// Stop server
 const stopServer = (server: Server) => {
   mongoose.connection.close();
   server.close(() => {
