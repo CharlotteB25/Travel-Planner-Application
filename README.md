8f0529c97fe31f50b5f384c995be5ad5e5ae23b0
Charlotte Billiet <charbill@student.arteveldehs.be>
2024-10-27 19:12:23 +0100

    27/10

--- README.md
+++
@@ -1,7 +0,0 @@
-# travel-app
-
-testing user information:
-email: hello@gmail.com
-password: hello123
-
-this is my MERN stack application for a travel planner
--- api/src/middleware/auth/localStrategy.ts
+++ api/src/middleware/auth/localStrategy.ts
@@ -1,30 +1,36 @@
-import { Strategy as LocalStrategy } from "passport-local";
+import { IVerifyOptions, Strategy as LocalStrategy } from "passport-local";
 import User from "../../modules/User/User.model";
 
+const localOptions = {
+  usernameField: "email",
+};
+
+// local strategy to check login with username and password
 const localStrategy = new LocalStrategy(
-  { usernameField: "email" },
-  async (email, password, done) => {
-    console.log("Authenticating user:", email); // Debugging line
-
-    try {
-      const user = await User.findOne({ email });
-      if (!user) {
-        console.log("User not found:", email); // Debugging line
-        return done(null, false, { message: "Incorrect email or password." });
+  localOptions,
+  (
+    email: string,
+    password: string,
+    done: (
+      error: any,
+      user?: Express.User | false,
+      options?: IVerifyOptions
+    ) => void
+  ) => {
+    (async () => {
+      try {
+        const user = await User.findOne({ email });
+        if (user) {
+          const check = await user.comparePassword(password);
+          if (check) {
+            return done(null, user);
+          }
+        }
+        return done(null);
+      } catch (e) {
+        return done(e);
       }
-
-      const isMatch = await user.comparePassword(password);
-      if (!isMatch) {
-        console.log("Password mismatch for user:", email); // Debugging line
-        return done(null, false, { message: "Incorrect email or password." });
-      }
-
-      console.log("User authenticated successfully:", user); // Debugging line
-      return done(null, user);
-    } catch (error) {
-      console.error("Error during authentication:", error); // Debugging line
-      return done(error);
-    }
+    })();
   }
 );
 
